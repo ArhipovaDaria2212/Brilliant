5615375c6e1009d6bb6438f9fd6a451ed03f00bf
Arhipova Daria <daria221220042017@gmail.com>
2024-06-05 18:27:30 +0700

    test

--- src/webapp/brilliant/src/config/axios_config.js
+++ src/webapp/brilliant/src/config/axios_config.js
@@ -7,7 +7,7 @@ axios.interceptors.response.use(response => {
     return response;
 }, error => {
     if (error.response?.status === 401) {
-        window.location.href = "/login";
+        // window.location.href = "/login";
     }
 
     return Promise.reject(error);
